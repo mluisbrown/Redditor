1b6c1dcf4b35450daae7d16b48766ae570652e3e
Michael Brown <michael@luacheia.com>
2014-07-04 19:34:07 +0100

    Initial Commit

---
+++ Redditor/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  Redditor
+//
+//  Created by Michael Brown on 04/07/2014.
+//  Copyright (c) 2014 Michael Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Redditor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Redditor
+//
+//  Created by Michael Brown on 04/07/2014.
+//  Copyright (c) 2014 Michael Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
